0410d1589d7069461ba1c6a2b9439d81635cc427
huof6829 <68298506@qq.com>
2022-02-17 19:56:01 +0800

    modify

--- hello/hello.go
+++ hello/hello.go
@@ -6,6 +6,20 @@ func Greet() string {
 }
 
 // Het ... make a test
+//nolint
 func Het() string {
+	return "Hello GitHub Actions"
+}
+
+// Get ... make a test
+func Get() string {
+	i := 10
+	if true {
+		i++
+		//nolint
+	} else {
+		i--
+		i += 1
+	}
 	return "Hello GitHub Actions"
 }
